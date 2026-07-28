e492ade2b204127c32c562fa988cf98dd1b93fee
hectorib2001 <hectorichasobarrientos@gmail.com>
2026-07-27 22:17:22 -0400

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/website/landing.css', 
+                'resources/js/website/landing.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
